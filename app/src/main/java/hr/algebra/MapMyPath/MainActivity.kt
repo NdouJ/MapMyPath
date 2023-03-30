c18a2c03ec91005c5109825fd366c7990483afec
NdouJ <jelena.ndou@gmail.com>
2023-03-30 19:27:52 +0200

    cardview for login and navigation fragment

--- app/src/main/java/hr/algebra/MapMyPath/MainActivity.kt
+++ app/src/main/java/hr/algebra/MapMyPath/MainActivity.kt
@@ -1,8 +1,10 @@
 package hr.algebra.MapMyPath
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 
@@ -12,10 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView = findViewById<ImageView>(R.id.myImageView)
-      //  imageView.setImageResource()
+        val buttonSkip: Button = findViewById(R.id.btn_skip)
 
-        Picasso.get().load("https://upload.wikimedia.org/wikipedia/commons/2/27/Wkipedia_blank_world_map.jpg").into(imageView)
+        val buttonSubmit : Button = findViewById(R.id.btn_submit)
+        buttonSkip.setOnClickListener {
+
+                startActivity(Intent(this, NavigationActivity::class.java))
+            }
+
+
+        buttonSubmit.setOnClickListener {
+
+            //TODO: firebase ili neka autentifikacija s dodatnim funkcionalnostima
+            startActivity(Intent(this, NavigationActivity::class.java))
+
+        }
+
 
     }
 }
