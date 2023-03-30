2d7d67396141e46bced08bdcd305fba4fa21166c
NdouJ <jelena.ndou@gmail.com>
2023-03-30 20:03:43 +0200

    Keyboard hiding

--- app/src/main/java/hr/algebra/MapMyPath/MainActivity.kt
+++ app/src/main/java/hr/algebra/MapMyPath/MainActivity.kt
@@ -1,12 +1,20 @@
 package hr.algebra.MapMyPath
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.squareup.picasso.Picasso
+
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -17,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         val buttonSkip: Button = findViewById(R.id.btn_skip)
 
         val buttonSubmit : Button = findViewById(R.id.btn_submit)
+
+        val rootView = findViewById<View>(android.R.id.content)
+
+
         buttonSkip.setOnClickListener {
 
                 startActivity(Intent(this, NavigationActivity::class.java))
@@ -29,6 +41,29 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, NavigationActivity::class.java))
 
         }
+
+        rootView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                val focusedView = currentFocus
+                if (focusedView != null) {
+                    if (focusedView is TextView) {
+                        val outRect = Rect()
+                        focusedView.getGlobalVisibleRect(outRect)
+                        if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                            focusedView.clearFocus()
+                            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            imm.hideSoftInputFromWindow(focusedView.windowToken, 0)
+                        }
+                    }
+                }
+            }
+            false
+        }
+
+
+
+
+
 
 
     }
