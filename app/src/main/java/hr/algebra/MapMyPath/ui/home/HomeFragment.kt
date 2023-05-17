7b0b2855cb2fc018301155963a5ed73bac1edac8
NdouJ <jelena.ndou@gmail.com>
2023-05-17 16:52:49 +0200

    camera functionality added

--- app/src/main/java/hr/algebra/MapMyPath/ui/home/HomeFragment.kt
+++ app/src/main/java/hr/algebra/MapMyPath/ui/home/HomeFragment.kt
@@ -1,16 +1,40 @@
 package hr.algebra.MapMyPath.ui.home
 
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import hr.algebra.MapMyPath.databinding.FragmentHomeBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.INTERNET
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
 
 class HomeFragment : Fragment() {
 
+
+    companion object{
+        private const val CAMERA_PERMISION_CODE =1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -33,8 +57,37 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.floatingCamera.setOnClickListener {
+            if(
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED
+
+            ){
+                takePhoto()
+
+            }else{
+ActivityCompat.requestPermissions(
+    requireActivity(),
+    arrayOf(android.Manifest.permission.CAMERA),
+    CAMERA_PERMISION_CODE
+)
+
+            }
+
+
+            }
+
+
         return root
     }
+
+    private fun takePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
